a3e5310f202f392380f7c0c38582f30607f83386
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-01-06 23:06:38 +0100

    Initial release

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// /* Required, but no C code needed. */
+import "C"
+
+import (
+	"encoding/json"
+
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+)
+
+func Handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
+	return "Hello, World!", nil
+}
